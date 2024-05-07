9b9169b995f80d010e4f3c8cd2df8249d75e5b73
Diegor897 <dchocano@ucm.es>
2024-05-07 14:35:25 +0200

    cambio de la imagen

--- src/main/resources/static/js/ajax-demo.js
+++ src/main/resources/static/js/ajax-demo.js
@@ -1,78 +1,118 @@
 
 // envio de mensajes con AJAX
 let b = document.getElementById("sendmsg");
-const campo = "";
-b.onclick = (e) => {
-    e.preventDefault();
-    go(b.parentNode.action, 'POST', {
-            message: document.getElementById("message").value
-        })
-        .then(d => console.log("La d tiene el valor: ", d))
-        .catch(e => console.log("sad", e))
-}
 
-// cómo pintar 1 mensaje (devuelve html que se puede insertar en un div)
-function renderMsg(msg) {
-    console.log("rendering: ", msg);
+if (b){
 
-    const fechaHora = new Date(msg.sent);
- 
-    const dia = fechaHora.getDate();
-    const mes = fechaHora.getMonth() + 1; 
- 
-    const diaFormateado = dia < 10 ? `0${dia}` : dia;
-    const mesFormateado = mes < 10 ? `0${mes}` : mes;
- 
-    const hora = fechaHora.getHours();
-    const minutos = fechaHora.getMinutes();
- 
-    const horaFormateada = hora < 10 ? `0${hora}` : hora;
-    const minutosFormateados = minutos < 10 ? `0${minutos}` : minutos;
- 
-    const fechaFormateada = `${diaFormateado}/${mesFormateado}<br>${horaFormateada}:${minutosFormateados}`; 
-    // Incorporamos el botón en el HTML del mensaje si es necesario
-    return `<div class="infoMensaje">
-                <p class="textoFrom">${msg.from}<br></p>
-                <p class="textoFecha">${fechaFormateada}:</p>
-                <p class="textoMensaje">${msg.text}</p> <!-- Mensaje separado en una clase diferente -->
-                <button class="Foroboton" onclick="reportMessage(${msg.id})">Reportar</button>
-            </div>`;
-}
-
-let idLiga = window.location.pathname.split("/").pop();
-
-function reportMessage(msgId) {
-    let url = `${config.rootUrl}/foro/${idLiga}`
-    go(url, "PUT", {
-        idMensaje: msgId
-    }).then(() => {
-        // Encuentra el toast en el DOM
-        const toastEl = document.getElementById("reportToast");
-
-        // // Usa Bootstrap para mostrar el toast
-        const toast = new bootstrap.Toast(toastEl);
-        toast.show(); // Muestra el toast
-
-    }).catch(error => {
-        console.error("Error al mostrar la notificacion:", error);
-    });
-}
-
-// pinta mensajes viejos al cargarse, via AJAX
-let messageDiv = document.getElementById("mensajes");
-let foroUrl = `${config.rootUrl}/mensajes/${idLiga}`;
-go(foroUrl, "GET").then(ms => {
-   console.log("Cargando mensajes antiguios", ms);
-   ms.forEach(m => messageDiv.insertAdjacentHTML("beforeend", renderMsg(m)));})
-
-// y aquí pinta mensajes según van llegando
-if (ws.receive) {
-   const oldFn = ws.receive; // guarda referencia a manejador anterior
-    ws.receive = (m) => {
-       oldFn(m); // llama al manejador anterior
-       messageDiv.insertAdjacentHTML("beforeend", renderMsg(m));
+    const campo = "";
+    b.onclick = (e) => {
+        e.preventDefault();
+        go(b.parentNode.action, 'POST', {
+                message: document.getElementById("message").value
+            })
+            .then(d => console.log("La d tiene el valor: ", d))
+            .catch(e => console.log("sad", e))
+    }
+    
+    // cómo pintar 1 mensaje (devuelve html que se puede insertar en un div)
+    function renderMsg(msg) {
+        console.log("rendering: ", msg);
+    
+        const fechaHora = new Date(msg.sent);
+     
+        const dia = fechaHora.getDate();
+        const mes = fechaHora.getMonth() + 1; 
+     
+        const diaFormateado = dia < 10 ? `0${dia}` : dia;
+        const mesFormateado = mes < 10 ? `0${mes}` : mes;
+     
+        const hora = fechaHora.getHours();
+        const minutos = fechaHora.getMinutes();
+     
+        const horaFormateada = hora < 10 ? `0${hora}` : hora;
+        const minutosFormateados = minutos < 10 ? `0${minutos}` : minutos;
+     
+        const fechaFormateada = `${diaFormateado}/${mesFormateado}<br>${horaFormateada}:${minutosFormateados}`; 
+        // Incorporamos el botón en el HTML del mensaje si es necesario
+        return `<div class="infoMensaje">
+                    <p class="textoFrom">${msg.from}<br></p>
+                    <p class="textoFecha">${fechaFormateada}:</p>
+                    <p class="textoMensaje">${msg.text}</p> <!-- Mensaje separado en una clase diferente -->
+                    <button class="Foroboton" onclick="reportMessage(${msg.id})">Reportar</button>
+                </div>`;
+    }
+    
+    let idLiga = window.location.pathname.split("/").pop();
+    
+    function reportMessage(msgId) {
+        let url = `${config.rootUrl}/foro/${idLiga}`
+        go(url, "PUT", {
+            idMensaje: msgId
+        }).then(() => {
+            // Encuentra el toast en el DOM
+            const toastEl = document.getElementById("reportToast");
+    
+            // // Usa Bootstrap para mostrar el toast
+            const toast = new bootstrap.Toast(toastEl);
+            toast.show(); // Muestra el toast
+    
+        }).catch(error => {
+            console.error("Error al mostrar la notificacion:", error);
+        });
+    }
+    
+    // pinta mensajes viejos al cargarse, via AJAX
+    let messageDiv = document.getElementById("mensajes");
+    let foroUrl = `${config.rootUrl}/mensajes/${idLiga}`;
+    go(foroUrl, "GET").then(ms => {
+       console.log("Cargando mensajes antiguios", ms);
+       ms.forEach(m => messageDiv.insertAdjacentHTML("beforeend", renderMsg(m)));})
+    
+    // y aquí pinta mensajes según van llegando
+    if (ws.receive) {
+       const oldFn = ws.receive; // guarda referencia a manejador anterior
+        ws.receive = (m) => {
+           oldFn(m); // llama al manejador anterior
+           messageDiv.insertAdjacentHTML("beforeend", renderMsg(m));
+        }
     }
 }
+
+ 
+  // click en botones de "usar como foto de perfil"
+document.querySelectorAll(".perfilable").forEach(o => {
+    o.onclick = e => {
+        e.preventDefault();
+        let url = o.parentNode.action;
+        let img = o.parentNode.parentNode.querySelector("img");
+        postImage(img, url, "photo").then(() => {
+            let cacheBuster = "?" + new Date().getTime();
+            document.querySelector("a.nav-link>img.iwthumb").src = url + cacheBuster;
+        });
+}});
+
+// refresca previsualizacion cuando cambias imagen
+document.querySelector("#f_avatar").onchange = e => {
+    let img = document.querySelector("#avatar");
+    let fileInput = document.querySelector("#f_avatar");
+    console.log(img, fileInput);
+    readImageFileData(fileInput.files[0], img);
+};
+console.log("El botón objetivo es ", document.querySelector("#postAvatar"));
+
+// click en boton de enviar avatar
+document.querySelector("#postAvatar").onclick = e => {
+    e.preventDefault();
+
+    let url = document.querySelector("#postAvatar").parentNode.action;
+    let img = document.querySelector("#avatar");
+    let file = document.querySelector("#f_avatar");
+    postImage(img, url, "photo").then(() => {
+        let cacheBuster = "?" + new Date().getTime();
+        document.querySelector("a.nav-link>img.iwthumb").src = url + cacheBuster;
+    });
+};
+
 
 // // ver https://openlibrary.org/dev/docs/api/books
 // // no requieren "api key", pero necesitas 1 consulta adicional por autor
@@ -114,33 +154,8 @@ if (ws.receive) {
 //    console.log("fetching ", imdb);
 //    fetchMovieData(imdb, document.querySelector("#poster"));
 // };
-// // click en botones de "usar como foto de perfil"
-// document.querySelectorAll(".perfilable").forEach(o => {
-//    o.onclick = e => {
-//        e.preventDefault();
-//        let url = o.parentNode.action;
-//        let img = o.parentNode.parentNode.querySelector("img");
-//        postImage(img, url, "photo").then(() => {
-//            let cacheBuster = "?" + new Date().getTime();
-//            document.querySelector("a.nav-link>img.iwthumb").src = url + cacheBuster;
-//        });
-// }});
 
-// // refresca previsualizacion cuando cambias imagen
-// document.querySelector("#f_avatar").onchange = e => {
-//    let img = document.querySelector("#avatar");
-//    let fileInput = document.querySelector("#f_avatar");
-//    console.log(img, fileInput);
-//    readImageFileData(fileInput.files[0], img);
-// };
-// // click en boton de enviar avatar
-// document.querySelector("#postAvatar").onclick = e => {
-//    e.preventDefault();
-//    let url = document.querySelector("#postAvatar").parentNode.action;
-//    let img = document.querySelector("#avatar");
-//    let file = document.querySelector("#f_avatar");
-//    postImage(img, url, "photo").then(() => {
-//        let cacheBuster = "?" + new Date().getTime();
-//        document.querySelector("a.nav-link>img.iwthumb").src = url + cacheBuster;
-//    });
-// };
+
+    
+
+
